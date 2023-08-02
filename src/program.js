fa23ca7a08416f8bde17d71132968c663b0b3089
jacintaph <jacintaphillips14@gmail.com>
2023-08-02 16:03:27 +1000

    Create program architecture setup

---
+++ src/program.js
@@ -0,0 +1,36 @@
+export class GameLoop {
+  constructor() {
+    this.fps = 60;
+    this.ctx = null;
+    this.cnv = null;
+    this.loop = null;
+  }
+
+  startScreen() {
+    this.toggleScreen("startScreen", true);
+  }
+
+  start() {
+    this.toggleScreen("startScreen", false);
+    this.toggleScreen("canvas", true);
+  }
+
+  config() {
+    this.toggleScreen("startScreen", false);
+    this.toggleScreen("config", true);
+  }
+
+  highScores() {
+    this.toggleScreen("startScreen", false);
+    this.toggleScreen("highscores", true);
+  }
+
+  toggleScreen(id, toggle) {
+    let element = document.getElementById(id);
+    if (toggle) {
+      element.classList.remove("hidden");
+    } else {
+      element.classList.add("hidden");
+    }
+  }
+}
